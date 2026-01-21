f21b98278a6cbe55c6087b7676077516a925f237
Sk Abdur Rohid <abdurrohid339@gmail.com>
2026-01-21 10:01:27 +0530

    Created API  for login,updateProfile,logout, Routes for routing  and middleware for  user authentication

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -42,3 +42,124 @@ export const register = async(req,res)=>{
         console.log(err);
     }
 }
+export const login = async(req,res)=>{
+    try{
+        const { email, password,role } = req.body;
+        if(!email || !password || !role){
+            return res.status(400).json({
+                message:"Some credentials is missing",
+                success: false
+            });
+        };
+
+        let user = await User.findOne({ email });
+        
+        if(!user){
+            return res.status(400).json({
+                message:"User does not exists",
+                success:false
+            });
+        };
+
+        const isPasswordMatch = await bcrypt.compare(password,user.password);
+
+        if(!isPasswordMatch){
+            return res.status(400).json({
+                message:"Invalid credentials",
+                success:false
+            });
+        };
+
+        if(role != user.role){
+            return res.status(400).json({
+                message:"Account doesn't with the correct role.",
+                success:false
+            });
+        };
+
+        const tokenData ={
+            userId : user._id
+        }
+        const token = await jwt.sign(tokenData, process.env.SECRET_KEY, {expiresIn:'1d'});
+
+         user = {
+            _id : user._id,
+            fullname: user.fullname,
+            email:user.email,
+            phoneNumber:user.phoneNumber,
+            role:user.role,
+        }
+
+        return res.status(200).cookie("token",token,{maxAge: 1*24*60*60*1000, httpOnly:true, sameSite:'strict'}).json({
+            message:`Welcome back ${user.fullname}`,
+            user,
+            success:true
+        });
+
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
+
+export const logout = async(req,res)=>{
+    try{
+        return res.status(200).cookie("token","",{maxAge: 0}).json({
+            message:"Logged out successfully",
+            success:true
+        })
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
+export const updateProfile = async(req,res)=>{
+    try{
+        const { fullname, email, phoneNumber,bio, skills } = req.body;
+        
+        let skillsArray;
+        if(skills){
+            skillsArray = skills.split(",");
+        }
+
+        const userId = req.id;//middleware authenticated
+        let user = await User.findById(userId);
+
+        if(!user){
+            return res.status(400).json({
+                message:"User not found",
+                success:false
+            });
+        };
+
+        //updating data
+        if(fullname) user.fullname= fullname
+        if(email) user.email= email
+        if(phoneNumber) user.phoneNumber= phoneNumber
+        if(bio) user.profile.bio= bio
+        if(skills) user.profile.skills= skillsArray
+
+        await user.save();
+
+        user ={
+            _id : user._id,
+            fullname:user.fullname,
+            email:user.email,
+            phoneNumber:user.phoneNumber,
+            role:user.role,
+            profile:user.profile
+        }
+
+        return res.status(200).json({
+            message: "Profile updated successfully",
+            user,
+            success: true
+        });
+
+    }
+    catch(err){
+        console.log(err);
+    }
+}
