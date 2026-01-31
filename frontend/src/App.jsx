248eac8e60ce8911097ecbda720fd9447932f25f
Sk Abdur Rohid <abdurrohid339@gmail.com>
2026-01-31 23:01:02 +0530

    updated frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './components/shared/navbar'
+import Home from './components/Home'
+import Login from './components/auth/Login'
+import Signup from './components/auth/Signup'
+
+
+
+
+// const appRouter = createBrowserRouter([
+//   {
+//     path:'/',
+//     element:<Home />
+//   },
+//   {
+//     path:'/login',
+//     element:<Login/>
+//   },
+//   {
+//     path:'/signup',
+//     element:<Signup/>
+//   }
+// ])
+
+function App() {
+
+  return (
+    <div>
+      {/* <RouterProvider router={appRouter}/> */}
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
