9d38a3df36132bae50bd5643bc38d47dbbf103d1
Sk Abdur Rohid <abdurrohid339@gmail.com>
2026-03-25 18:59:57 +0530

    updation in end point

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,6 @@
-export const USER_API_END_POINT = "http://localhost:3000/user";
-export const JOB_API_END_POINT = "http://localhost:3000/job";
-export const APPLICATION_API_END_POINT = "http://localhost:3000/application";
-export const COMPANY_API_END_POINT = "http://localhost:3000/company";
+export const USER_API_END_POINT = "https://job-pportal.onrender.com/user";
+export const JOB_API_END_POINT = "https://job-pportal.onrender.com/job";
+export const APPLICATION_API_END_POINT = "https://job-pportal.onrender.com/application";
+export const COMPANY_API_END_POINT = "https://job-pportal.onrender.com/company";
+
+//
