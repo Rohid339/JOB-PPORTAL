db438ef05694cf9d5de25643d80253922405f910
Sk Abdur Rohid <abdurrohid339@gmail.com>
2026-03-25 19:54:26 +0530

    path

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -113,7 +113,8 @@ export const login = async(req,res)=>{
             maxAge: 1 * 24 * 60 * 60 * 1000,
             httpOnly: true,
             secure: true,        // ✅ MUST for HTTPS (Render)
-            sameSite: "none"     // ✅ MUST for cross-origin
+            sameSite: "none",    // ✅ MUST for cross-origin
+            path:"/"
         })
         .json({
             message: `Welcome back ${user.fullname}`,
