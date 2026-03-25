f68d4f4910a022ad52c829a66829ec7e281f3071
Sk Abdur Rohid <abdurrohid339@gmail.com>
2026-03-25 19:48:35 +0530

    end point

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOption = {
     origin: [
-        "http://localhost:5173",
+        // "http://localhost:5173",
         "https://job-pportal-v6dq.onrender.com"
     ],
     credentials: true
